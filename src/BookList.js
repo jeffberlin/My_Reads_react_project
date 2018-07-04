3ac1971908b721b198298bd4aedeef8257fd7b1b
Jeff Berlin <jeff@jbrally.com>
2018-07-04 11:36:03 -0400

    Install dependencies, setting up pages

---
+++ src/BookList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+// This page handles the listing of the books on their chosen shelves
+class BookList extends Component {
+
+}
+
+export default BookList
