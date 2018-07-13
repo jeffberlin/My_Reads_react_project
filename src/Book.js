02be1be3bb1c0750a27d5a5e013d2dc191e534cd
Jeff Berlin <jeff@jbrally.com>
2018-07-13 19:11:58 -0400

    Fixed default shelf value

--- src/Book.js
+++ src/Book.js
@@ -21,7 +21,7 @@ class Book extends Component {
           <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : 'http://via.placeholder.com/128x193?text=No%20Cover' })` }}>
           </div>
           <div className='book-shelf-changer'>
-            <select value={book.shelf} onChange={(e) => this.updateBook(e.target.value)}>
+            <select value={book.shelf||"none"} onChange={(e) => this.updateBook(e.target.value)}>
               <option value="" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
